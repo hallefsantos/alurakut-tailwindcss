279ebb312f6c109e76140a7e9e3e73d7cb9d50b2
Hallef Santos <hallefsantos@gmail.com>
2021-07-15 16:59:29 -0300

    Add Card component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,16 @@
 import ProfileAvatar from "../components/ProfileAvatar"
+import Card from "../components/Card"
+
 export default function Home() {
   return (
-    <div className="min-h-screen bg-backgroundDefault">
+    <div className="p-10 min-h-screen bg-backgroundDefault">
       <h2>Profile component</h2>
       <ProfileAvatar />
+
+      <h2>Card component</h2>
+      <Card>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore laborum eum voluptate maxime maiores possimus saepe, officia cupiditate placeat. Veniam consectetur amet eaque hic animi molestias dolorum fugiat maiores blanditiis.
+      </Card>
     </div>
   )
 }
